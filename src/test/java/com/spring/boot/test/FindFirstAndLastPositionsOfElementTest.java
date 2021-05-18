7e3b611d2bffc42ef5423bc820f03208e8b6bd58
Hyungguen Han <hyungguen.han@rakuten.com>
2021-05-18 21:22:08 +0900

    refactor the binary search algorithm

--- src/test/java/com/spring/boot/test/FindFirstAndLastPositionsOfElementTest.java
+++ src/test/java/com/spring/boot/test/FindFirstAndLastPositionsOfElementTest.java
@@ -8,25 +8,25 @@ public class FindFirstAndLastPositionsOfElementTest {
     @Test
     public void test() {
 
-        int arr[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+        int arr[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
         findFirstAndLastPosition(arr, 8);
     }
 
     private void findFirstAndLastPosition(int[] arr, int element) {
 
-        if(arr == null || arr.length == 0) {
+        if (arr == null || arr.length == 0) {
             return;
         }
 
         int first = -1;
         int last = -1;
 
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] != element) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != element) {
                 continue;
             }
 
-            if(first == -1) {
+            if (first == -1) {
                 first = i;
             }
 
@@ -40,7 +40,7 @@ public class FindFirstAndLastPositionsOfElementTest {
     @Test
     public void test2() {
 
-        int arr[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+        int arr[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
         int first = first2(arr, 0, arr.length - 1, 8, arr.length);
         System.out.println("-------------------------------");
         int last = last2(arr, 0, arr.length - 1, 8, arr.length);
@@ -50,7 +50,7 @@ public class FindFirstAndLastPositionsOfElementTest {
         Assertions.assertEquals(first, 8);
         Assertions.assertEquals(last, 9);
 
-        int arr2[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+        int arr2[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
         int first2 = first(arr2, 0, arr2.length - 1, 2, arr2.length);
         int last2 = last(arr2, 0, arr2.length - 1, 2, arr2.length);
 
@@ -59,7 +59,7 @@ public class FindFirstAndLastPositionsOfElementTest {
         Assertions.assertEquals(first2, 1);
         Assertions.assertEquals(last2, 4);
 
-        int arr3[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+        int arr3[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
         int first3 = first(arr3, 0, arr3.length - 1, 9, arr3.length);
         int last3 = last(arr3, 0, arr3.length - 1, 9, arr3.length);
 
@@ -71,12 +71,12 @@ public class FindFirstAndLastPositionsOfElementTest {
 
     private int first(int[] arr, int low, int high, int element, int length) {
 
-        if(high < low) {
+        if (high < low) {
             return -1;
         }
 
         System.out.println("First Method...");
-        for(int i = low; i <= high; i++) {
+        for (int i = low; i <= high; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
@@ -84,10 +84,10 @@ public class FindFirstAndLastPositionsOfElementTest {
         int mid = low + (high - low) / 2;
         System.out.println("Mid: " + mid);
 
-        if((mid == 0 || element > arr[mid - 1]) && arr[mid] == element) {
+        if ((mid == 0 || element > arr[mid - 1]) && arr[mid] == element) {
             return mid;
 
-        } else if(element > arr[mid]) {
+        } else if (element > arr[mid]) {
             return first(arr, mid + 1, high, element, length);
 
         } else {
@@ -97,12 +97,12 @@ public class FindFirstAndLastPositionsOfElementTest {
 
     private int last(int[] arr, int low, int high, int element, int length) {
 
-        if(high < low) {
+        if (high < low) {
             return -1;
         }
 
         System.out.println("Last Method...");
-        for(int i = low; i <= high; i++) {
+        for (int i = low; i <= high; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
@@ -110,7 +110,7 @@ public class FindFirstAndLastPositionsOfElementTest {
         int mid = low + (high - low) / 2;
         System.out.println("Mid: " + mid);
 
-        if((mid == length - 1 || element < arr[mid + 1]) && arr[mid] == element) {
+        if ((mid == length - 1 || element < arr[mid + 1]) && arr[mid] == element) {
             return mid;
 
         } else if (element < arr[mid]) {
@@ -123,16 +123,20 @@ public class FindFirstAndLastPositionsOfElementTest {
 
     private int first2(int[] arr, int low, int high, int target, int length) {
 
-        if(high < low) {
+        if (high < low) {
             return -1;
         }
 
         int mid = low + ((high - low) / 2);
 
-        if ((mid == 0 && target == arr[mid]) || (target > arr[mid - 1] && target == arr[mid])) {
+        if (mid == 0 && target == arr[mid]) {
+            return mid;
+        }
+
+        if (target > arr[mid - 1] && target == arr[mid]) {
             return mid;
 
-        } else if(target > arr[mid]) {
+        } else if (target > arr[mid]) {
             return first2(arr, (mid + 1), high, target, length);
 
         } else {
@@ -142,16 +146,20 @@ public class FindFirstAndLastPositionsOfElementTest {
 
     private int last2(int[] arr, int low, int high, int target, int length) {
 
-        if(high < low) {
+        if (high < low) {
             return -1;
         }
 
         int mid = low + ((high - low) / 2);
 
-        if((mid == length - 1 && target == arr[mid]) || (target < arr[mid + 1] && target == arr[mid])) {
+        if (mid == length - 1 && target == arr[mid]) {
+            return mid;
+        }
+
+        if (target < arr[mid + 1] && target == arr[mid]) {
             return mid;
 
-        } else if(target < arr[mid]) {
+        } else if (target < arr[mid]) {
             return last2(arr, low, (mid - 1), target, length);
 
         } else {
