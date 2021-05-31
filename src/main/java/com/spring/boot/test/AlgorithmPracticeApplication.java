fbc087bea0726d090f5d8564dc7a348461621423
Hyungguen Han <hyungguen.han@rakuten.com>
2021-06-01 03:12:55 +0900

    change project name

--- src/main/java/com/spring/boot/test/AlgorithmPracticeApplication.java
+++ src/main/java/com/spring/boot/test/AlgorithmPracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
+public class AlgorithmPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class, args);
+        SpringApplication.run(AlgorithmPracticeApplication.class, args);
     }
 
 }
