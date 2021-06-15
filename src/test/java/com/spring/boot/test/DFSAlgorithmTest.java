af5f8e6bc26c434c1350ba0177e7523c906ad28f
Hyungguen Han <hyungguen.han@rakuten.com>
2021-06-16 01:31:39 +0900

    add DFS algorithm test

---
+++ src/test/java/com/spring/boot/test/DFSAlgorithmTest.java
@@ -0,0 +1,62 @@
+package com.spring.boot.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class DFSAlgorithmTest {
+
+    @Test
+    public void test() {
+
+        Graph2 g = new Graph2(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.dfs(2);
+    }
+
+    class Graph2 {
+
+        private int vertices;
+
+        private LinkedList<Integer>[] adjs;
+
+        Graph2(int vertices) {
+            this.vertices = vertices;
+            adjs = new LinkedList[vertices];
+            for (int i = 0; i < vertices; i++) {
+                adjs[i] = new LinkedList<>();
+            }
+        }
+
+        public void addEdge(int vertice, int val) {
+            adjs[vertice].add(val);
+        }
+
+        public void dfs(int startPoint) {
+
+            dfs(startPoint, new boolean[vertices]);
+        }
+
+        public void dfs(int point, boolean[] visited) {
+
+            visited[point] = true;
+            System.out.print(point + " ");
+            ListIterator<Integer> list = adjs[point].listIterator();
+            while (list.hasNext()) {
+                int val = list.next();
+                while (!visited[val]) {
+                    dfs(val, visited);
+                }
+            }
+        }
+    }
+}
