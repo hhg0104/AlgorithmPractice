9c4e2f918c2e504bdca1471ac6f31d4b6ba3c20b
Hyungguen Han <hyungguen.han@rakuten.com>
2021-07-19 00:09:07 +0900

    add a new logic for LongestCommonPrefixTest

--- src/test/java/com/spring/boot/test/LongestCommonPrefixTest.java
+++ src/test/java/com/spring/boot/test/LongestCommonPrefixTest.java
@@ -32,11 +32,14 @@ public class LongestCommonPrefixTest {
     @Test
     public void test() {
 
-        String actual1 = longestCommonPrefix(new String[]{"flower", "flow", "flight"});
+        String actual1 = longestCommonPrefix2(new String[]{"flower", "flow", "flight"});
         Assertions.assertEquals("fl", actual1);
 
-        String actual2 = longestCommonPrefix(new String[]{"dog", "racecar", "car"});
+        String actual2 = longestCommonPrefix2(new String[]{"dog", "racecar", "car"});
         Assertions.assertEquals("", actual2);
+
+        String actual3 = longestCommonPrefix2(new String[]{"ab", "a"});
+        Assertions.assertEquals("a", actual3);
     }
 
     public String longestCommonPrefix(String[] strs) {
@@ -55,5 +58,39 @@ public class LongestCommonPrefixTest {
         }
 
         return strs[0];
+    }
+
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String firstStr = strs[0];
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        while (idx < firstStr.length()) {
+            char ch = firstStr.charAt(idx);
+            boolean isMatched = true;
+            for (int i = 1; i < strs.length; i++) {
+                if (idx >= strs[i].length()) {
+                    return sb.toString();
+                }
+
+                if (ch != strs[i].charAt(idx)) {
+                    isMatched = false;
+                    break;
+                }
+            }
+
+            if (isMatched) {
+                sb.append(ch);
+            } else {
+                return sb.toString();
+            }
+
+            idx++;
+        }
+
+        return sb.toString();
     }
 }
